1ab90da0f8d79d3a8687074329ed55c115d942c8
Nils Larsgård <nilsmagnus@gmail.com>
2017-03-30 15:54:56 +0200

    start template 4.8

--- griblib/products.go
+++ griblib/products.go
@@ -19,6 +19,24 @@ type Product0 struct {
 	SecondSurface Surface
 }
 
+// http://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_temp4-8.shtml
+type Product8 struct {
+	ParameterCategory uint8
+	ParameterNumber   uint8
+	ProcessType       uint8
+	BackgroundProcess uint8
+	AnalysisProcess   uint8
+	Hours             uint16
+	Minutes           uint8
+	TimeUnitIndicator uint8
+	ForecastTime      uint32
+
+	FirstSurface  Surface
+	SecondSurface Surface
+	Time          Time
+	// TODO continue on byte 42
+}
+
 type Surface struct {
 	Type  uint8
 	Scale uint8
