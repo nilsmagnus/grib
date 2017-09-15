21464f85d0cfb42a3c04934e88d4f1fc595901e5
Nils Larsgård <nilsmagnus@gmail.com>
2017-09-15 08:12:50 +0000

    Remove useless testdata script

--- gettestdata.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir testdata
-date="2017032200"
-for hour in "000" "003" "006" "009" "012"
-do
-    wget -O testdata/gfs.t00z.pgrb2.2p50.f$hour http://www.ftp.ncep.noaa.gov/data/nccf/com/gfs/prod/gfs.$date/gfs.t00z.pgrb2.2p50.f$hour
-done
